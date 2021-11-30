8314662dc828009fa142c8994e9272f9d80ef5ab
ARBIND LOCHAN MISHRA <56961555+Arbind007@users.noreply.github.com>
2021-11-30 18:00:29 +0530

    Create 119A - Epic Game.cpp

---
+++ 119A - Epic Game.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int GCD(int m, int n)
+{
+    int r;
+    while ((r = m % n) != 0)
+    {
+        m = n;
+        n = r;
+    }
+    return n;
+}
+ 
+int main()
+{
+    int a, b, n;
+    cin >> a >> b >> n;
+    while (1)
+    {
+        n -= GCD(a, n);
+        if (n == 0)
+        {
+            cout << 0 << endl;
+            break;
+        }
+        n -= GCD(b, n);
+        if (n == 0)
+        {
+            cout << 1 << endl;
+            break;
+        }
+    }
+    return 0;
+}
