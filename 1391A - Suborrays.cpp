b0bfd3ba67efff8f2d7039d152877a92c6f581ca
ARBIND LOCHAN MISHRA <56961555+Arbind007@users.noreply.github.com>
2021-12-08 14:52:01 +0530

    Create 1391A - Suborrays.cpp

---
+++ 1391A - Suborrays.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        int j = n;
+        for(int i=1; i<=n; i++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
