1be8c248bed525d5d12c2edad4822462e356206e
ARBIND LOCHAN MISHRA <56961555+Arbind007@users.noreply.github.com>
2021-12-21 16:30:21 +0530

    Create 34B - Sale.cpp

---
+++ 34B - Sale.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int n,m;
+    cin >> n >> m;
+    vector <int> v(n);
+    for(auto &i : v){
+        cin >> i;
+    }
+    sort(v.begin(),v.end());
+    int sum = 0;
+    for(int i=0;i<m;i++){
+       
+        if(v[i]<0){
+            sum += v[i];
+        }
+        else{
+            break;
+        }
+    }
+    cout << -sum;
+    return 0;
+}
