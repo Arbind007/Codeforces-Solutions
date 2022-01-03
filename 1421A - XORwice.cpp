fdf627740832f734386e007732b526649ce6c207
ARBIND LOCHAN MISHRA <56961555+Arbind007@users.noreply.github.com>
2022-01-03 19:21:58 +0530

    Create 1421A - XORwice.cpp

---
+++ 1421A - XORwice.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t,a,b,c;
+    cin >> t;
+    while(t--){
+        cin >> a >> b;
+        c = a xor b;
+        cout << c << endl;
+    }
+    return 0;
+}
