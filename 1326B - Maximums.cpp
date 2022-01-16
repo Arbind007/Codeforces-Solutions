5ef069ba4b54437e77e928de9a17bcbf1d00160e
ARBIND LOCHAN MISHRA <56961555+Arbind007@users.noreply.github.com>
2022-01-16 17:38:17 +0530

    Update 1326B - Maximums.cpp

--- 1326B - Maximums.cpp
+++ 1326B - Maximums.cpp
@@ -1,20 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+ 
 int main()
 {
     int n, m = 0, a;
     cin>>n;
     for(int i=0; i<n; i++){
         cin>>a;
-        if(i == 0){
-            cout<<a<<" ";
-            m = a;
-        }
-        else{
-            cout<<a+m<<" ";
-            m = max(m,a+m);
-        }
+        cout<<a+m<<" ";
+        m = max(m,a+m);
     }
     return 0;
 }
