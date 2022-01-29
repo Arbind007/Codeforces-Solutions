26ac08b22aec49828a3c0d373b1eb22596a45924
ARBIND LOCHAN MISHRA <56961555+Arbind007@users.noreply.github.com>
2022-01-29 18:25:19 +0530

    Create 1422A - Fence.cpp

---
+++ 1422A - Fence.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve(){
+    long long a,b,c,ans;
+    cin>>a>>b>>c;
+    ans=(a+b+c-1);
+    cout<<ans<<endl;
+}
+ 
+int main(){
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+    return 0;
+}
