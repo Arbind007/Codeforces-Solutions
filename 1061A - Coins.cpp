67d2a3977c276d5d44ca71abaefb8b24de00c7d4
ARBIND LOCHAN MISHRA <56961555+Arbind007@users.noreply.github.com>
2022-01-31 17:56:26 +0530

    Create 1061A - Coins.cpp

---
+++ 1061A - Coins.cpp
@@ -0,0 +1,14 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n,s,r;
+    cin>>n>>s;
+    if(s%n==0)
+        cout<<s/n<<endl;
+    else
+        cout<<(s/n)+1<<endl;
+    return 0;
+}
