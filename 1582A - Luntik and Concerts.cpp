4697c626a83792aa0f5f257ddf7fc46505af0a66
ARBIND LOCHAN MISHRA <56961555+Arbind007@users.noreply.github.com>
2022-02-28 18:35:07 +0530

    Create 1582A - Luntik and Concerts.cpp

---
+++ 1582A - Luntik and Concerts.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t,a,b,c;
+    cin >> t;
+    while(t--){
+        cin >> a >> b >> c;
+        cout << (a+c)%2 << endl;
+    }
+    return 0;
+}
